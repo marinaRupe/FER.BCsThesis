0982c08c9f1c164c40f7cdd02a3aa1a9690b0c35
marinaRupe <marina.rupe@fer.hr>
2017-12-22 16:19:03 +0100

    Upload thesis (in Croatian), update main function

--- PathogenAnalyzer.py
+++ PathogenAnalyzer.py
@@ -5,18 +5,25 @@ from EMAlgorithm import EMAlgorithm
 from DatabaseReducer import DatabaseReducer
 from res.ResourceFiles import REDUCED_DB_FILE
 
-try:
-    INPUT_FILE = str(argv[1])
-except:
-    print("Missing argument: input file")
-    exit(1)
 
-reducedDbPath = Path(REDUCED_DB_FILE)
-if not reducedDbPath.is_file():
-    DatabaseReducer().generate()
+def main():
+    try:
+        INPUT_FILE = str(argv[1])
+    except:
+        print("Missing argument: input file")
+        exit(1)
 
-OUTPUT_FILE_NAME = ''.join(INPUT_FILE.split('/')[-1].split('.')[:-1]) + ".sam"
-ALIGNMENTS_FILE = "alignments/out/" + OUTPUT_FILE_NAME
+    if not Path(REDUCED_DB_FILE).is_file():
+        DatabaseReducer().generate()
 
-system("graphmap align -r " + "./" + REDUCED_DB_FILE + " -d " + INPUT_FILE + " -o " + "./" + ALIGNMENTS_FILE)
-EM = EMAlgorithm().start(ALIGNMENTS_FILE)
+    OUTPUT_FILE_NAME = ''.join(INPUT_FILE.split('/')[-1].split('.')[:-1]) + ".sam"
+    ALIGNMENTS_FILE = "alignments/out/" + OUTPUT_FILE_NAME
+
+    if not Path(ALIGNMENTS_FILE).is_file():
+        system("graphmap align -r " + "./" + REDUCED_DB_FILE + " -d " + INPUT_FILE + " -o " + "./" + ALIGNMENTS_FILE)
+
+    EMAlgorithm().start(ALIGNMENTS_FILE)
+
+
+if __name__ == "__main__":
+    main()
